b4893a053bf22d403c3f657557de086bdc6ea1e8
Sengthai Te <sengthaite@gmail.com>
2023-01-08 23:13:40 +0700

    add handler scientific notation atof

--- atof.c
+++ atof.c
@@ -14,17 +14,33 @@ double atof(char s[])
         i++;
     for (val = 0.0; isdigit(s[i]); i++)
         val = 10.0 * val + (s[i] - '0');
-    if (s[i] == '.')
-        i++;
+    i++;
     for (power = 1.0; isdigit(s[i]); i++) {
         val = 10.0 * val + (s[i] - '0');
         power *= 10;
     }
+    int exp = 0, cur, mul = 1;
+    char exp_sign;
+    if (s[i++] == 'e')
+        while((cur = s[i]) != '\0')
+        {
+            i++;
+            if (cur == '-') {
+                exp_sign = '-';
+                continue;
+            }
+            exp = exp * mul + (cur - '0');
+            mul *= 10;
+        }
+    while (exp-- > 0)
+        power = exp_sign == '-' ? power * 10 : power / 10;
+
+    
     return sign * val / power;
 }
 
 int main(void)
 {
-    printf("%s to double = %f\n", "123.45", atof("123.45"));
+    printf("%s to double = %f\n", "123.45e-2", atof("123.45e-2"));
     return 0;
 }
