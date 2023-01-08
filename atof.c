9c61d08bf4dc55b857ee076e2569bbe44bbabc93
sengthaite <sengthaite@gmail.com>
2023-01-08 16:39:49 +0700

    add atof

---
+++ atof.c
@@ -0,0 +1,30 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/* atof: convert string s to double */
+double atof(char s[])
+{
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i]); i++) /* skip white space */
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.')
+        i++;
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10;
+    }
+    return sign * val / power;
+}
+
+int main(void)
+{
+    printf("%s to double = %f\n", "123.45", atof("123.45"));
+    return 0;
+}
