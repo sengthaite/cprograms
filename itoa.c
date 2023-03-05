c4a3de95aa560aceac00c0a41a89b7bf81738604
sengthaite <sengthaite@gmail.com>
2023-03-05 13:01:51 +0700

    update itoa pointer

--- itoa.c
+++ itoa.c
@@ -4,14 +4,16 @@
 
 void reverse(char s[]);
 void itoa(int n, char s[]);
-void _itoa(int, char *);
+void itoap(int n, char *s);
 
 int main(void)
 {
     char result[1000];
     printf("INT_MAX = %d\n", INT_MAX);
-    itoa(INT_MAX, result);
-    printf("itoa(INT_MAX) = %s\n", result);
+    // itoa(INT_MAX, result);
+    // printf("itoa(INT_MAX) = %s\n", result);
+    itoap(INT_MAX, result);
+    printf("result: %s\n", result);
     return 0;
 }
 
@@ -37,4 +39,21 @@ void itoa(int n, char s[])
         s[i++] = '-';
     s[i] = '\0';
     reverse(s);
+}
+
+void itoap(int n, char *s) {
+    char *p = s;
+    int is_negative = 0;
+    if (n < 0) {
+        n = -n;
+        is_negative = 1;
+    }
+    while (n > 0) {
+        *s++ = n % 10 + '0';
+        n /= 10;
+    }
+    if (is_negative)
+        *s++ = '-';
+    *s = '\0';
+    reverse(p);
 }
